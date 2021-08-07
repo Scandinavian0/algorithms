3b6d01bddfaef30d4dc29a8e6b8e96ab25b84700
Everglow <948719668@qq.com>
2021-08-07 18:22:41 +0800

    Initial commit

---
+++ src/java/alg/a_4_4_st/DeDup.java
@@ -0,0 +1,27 @@
+package alg.a_4_4_st;
+
+import alg.std.StdIn;
+import alg.std.StdOut;
+
+/******************************************************************************
+ *  Compilation:  javac DeDup.java
+ *  Execution:    java DeDup < words.txt
+ *  Dependencies: SET.java StdIn.java
+ *
+ *  Read in a list of words from standard input and print out
+ *  each word, removing any duplicates.
+ *
+ ******************************************************************************/
+
+public class DeDup {  
+    public static void main(String[] args) {
+        SET<String> distinct = new SET<String>();
+        while (!StdIn.isEmpty()) {
+            String key = StdIn.readString();
+            if (!distinct.contains(key)) {
+                distinct.add(key);
+                StdOut.println(key);
+            }
+        }
+    }
+}
